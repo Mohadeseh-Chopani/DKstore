6b8f8176af9c2059fb9aed2a8caaadc57a35c7fa
Mohadeseh chopani <chopanimohadeseh0@gmail.com>
2025-07-29 10:32:40 +0330

    Edite color file and change theme in theme file

--- app/src/main/java/com/example/digikala/ui/theme/Theme.kt
+++ app/src/main/java/com/example/digikala/ui/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color // 👈 این خط را اضافه کنید
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -20,16 +21,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -39,7 +30,7 @@ fun DigikalaTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    val baseColorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -49,8 +40,22 @@ fun DigikalaTheme(
         else -> LightColorScheme
     }
 
+    // if them was light, all colors change to pure white
+    val appColorScheme = if (!darkTheme) {
+        baseColorScheme.copy(
+            surface = Color.White,
+            surfaceTint = Color.White, //this line is very important
+            surfaceContainerLow = Color.White,
+            surfaceContainer = Color.White,
+            surfaceContainerHigh = Color.White,
+            background = Color.White
+        )
+    } else {
+        baseColorScheme
+    }
+
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = appColorScheme,
         typography = Typography,
         content = content
     )
